6244272347d44ff818b6d28222311e00715f70b5
Alex <muhammaddafa6279@gmail.com>
2024-06-02 23:23:37 +0700

    portofolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'selector',
+  darkMode: 'class',
   content: ['"./**/*.{html,js}"'],
   theme: {
 
     extend: {
       colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+        'gelap':'#121212',
+        
       }
     },
     fontFamily: {
